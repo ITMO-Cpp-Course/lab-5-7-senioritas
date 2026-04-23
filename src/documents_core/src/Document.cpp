028e1a8a4fa71888fd3e6a4dfa3a2ac9d8e98217
Lujekiell <aleksapruzh@gmail.com>
2026-04-23 16:08:49 +0300

    3nd Sasha's changes with all hpp (senioritas, check if you like structure pls)

--- src/documents_core/src/Document.cpp
+++ src/documents_core/src/Document.cpp
@@ -2,21 +2,25 @@
 #include <stdexcept>
 #include <string>
 
-void DocumentStorage::Save(Document&& doc) {
-   DocId id = doc.id;
+void DocumentStorage::Save(Document&& doc)
+{
+    DocId id = doc.id;
     data_[id] = std::move(doc);
 }
 
-const Document& DocumentStorage::Get(DocId id) const {
+const Document& DocumentStorage::Get(DocId id) const
+{
     auto it = data_.find(id);
 
-    if (it == data_.end()) {
+    if (it == data_.end())
+    {
         throw std::out_of_range("DocumentStorage: Document with ID " + std::to_string(id) + " not found.");
     }
     return it->second;
 }
 
-void DocumentStorage::Remove(DocId id) {
+void DocumentStorage::Remove(DocId id)
+{
     // Удаление из unordered_map по ключу
     data_.erase(id);
 }
