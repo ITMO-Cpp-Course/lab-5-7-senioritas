63122e0c19b610ea6d6683e54d8039db42fdfa02
Th4753 <te692734@gmail.com>
2026-04-26 13:18:42 +0300

    Document Builder

--- src/documents_core/include/lab5/documents/DocumentBuilder.hpp
+++ src/documents_core/include/lab5/documents/DocumentBuilder.hpp
@@ -7,7 +7,7 @@ class DocumentBuilder
 {
   public:
     // Создает объект Document, приводя текст к нижнему регистру
-    static Document Build(size_t id, std::string name, std::string content);
+    static Document Build(size_t id, const std::string& name, std::string content);
 
     // Разбивает текст на слова, убирая знаки препинания
     static std::vector<std::string> Tokenize(const std::string& text);
--- src/documents_core/src/DocumentBuilder.cpp
+++ src/documents_core/src/DocumentBuilder.cpp
@@ -1,16 +1,48 @@
-#include <algorithm>
-#include <cerrno>
-#include <cmath>
-#include <cstring>
-#include <fcntl.h>
-#include <iostream>
-#include <unistd.h>
-#include <vector>
-#include <lab5/documents/Document.hpp>
-#include <lab5/documents/DocumentBuilder.hpp>
-#include <lab5/documents/InvertedIndex.hpp>
-//or your includes that neccessary
-namespace lab5::documents
-{
+#include "lab5/documents/DocumentBuilder.hpp"
+#include <cctype>
 
-};
+Document DocumentBuilder::Build(size_t id, const std::string& name, std::string content)
+{
+    for (char& c : content)
+    {
+        c = static_cast<char>(std::tolower(c));
+    }
+    Document doc;
+    doc.id = id;
+    doc.name = name;
+    doc.content = content;
+    return doc;
+}
+
+std::vector<std::string> DocumentBuilder::Tokenize(const std::string& text)
+{
+    std::vector<std::string> tokens;
+    std::string word;
+
+    for (char c : text)
+    {
+        if (std::isalnum(static_cast<unsigned char>(c)))
+        { // если английская буква или число
+            word += c;
+        }
+        else if (!word.empty())
+        {
+            tokens.push_back(Normalize(word)); // добаляем в итоговый вектор
+            word.clear();
+        }
+    }
+    if (!word.empty())
+    {
+        tokens.push_back(Normalize(word));
+    }
+    return tokens;
+}
+
+std::string DocumentBuilder::Normalize(std::string word)
+{
+    for (char& c : word)
+    {
+        c = static_cast<char>(std::tolower(c));
+    }
+    return word;
+}
