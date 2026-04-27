ecdb82ac81639ac60670e9d85f70eddf7334a67e
Lujekiell <aleksapruzh@gmail.com>
2026-04-27 16:12:16 +0300

    final version

--- src/documents_core/include/lab5/documents/DocumentBuilder.hpp
+++ src/documents_core/include/lab5/documents/DocumentBuilder.hpp
@@ -7,13 +7,10 @@ namespace lab5::documents
 class DocumentBuilder
 {
   public:
-    // Создает объект Document, приводя текст к нижнему регистру
     static Document Build(size_t id, const std::string& name, std::string content);
 
-    // Разбивает текст на слова, убирая знаки препинания
     static std::vector<std::string> Tokenize(const std::string& text);
 
-    // Вспомогательная функция для очистки слова
     static std::string Normalize(std::string word);
 };
 } // namespace lab5::documents
--- src/documents_core/include/lab5/documents/InvertedIndex.hpp
+++ src/documents_core/include/lab5/documents/InvertedIndex.hpp
@@ -10,20 +10,15 @@ namespace lab5::documents
 class InvertedIndex
 {
   public:
-    // Принимает ID документа и список очищенных слов
     void AddDocument(size_t doc_id, const std::vector<std::string>& words);
 
-    // Удаляет упоминания документа из всех индексов
     void RemoveDocument(size_t doc_id);
 
-    // Получить по слову количество вхожлений в каждом документе и общее количество вхождений
     std::pair<std::map<size_t, size_t>, size_t> GetResultsForWord(std::string word);
 
-    // Получить по слову список документов, в которые он входит
     std::vector<size_t> GetListOfDocementsForWord(std::string word);
 
   private:
-    // Слово -> { ID документа -> количество вхождений }
     std::unordered_map<std::string, std::map<size_t, size_t>> index_;
 };
 } // namespace lab5::documents
