6a26eae3d8cbb1f6060a391a14c87cdbf0e4861d
Th4753 <te692734@gmail.com>
2026-05-08 17:49:50 +0300

    UpdateTransaction

--- src/documents_core/include/lab5/documents/UpdateTransaction.hpp
+++ src/documents_core/include/lab5/documents/UpdateTransaction.hpp
@@ -4,37 +4,39 @@
 #include "InvertedIndex.hpp"
 #include "Result.hpp"
 #include <set>
-#include <vector>
 #include <string>
+#include <vector>
 
-namespace lab5::documents {
+namespace lab5::documents
+{
 
-class UpdateTransaction {
-public:
-    //Конструктор создает локальную копию данных из IndexStore
-    //Здесь происходит инициализация временных транзакций
+class UpdateTransaction
+{
+  public:
+    // Конструктор создает локальную копию данных из IndexStore
+    // Здесь происходит инициализация временных транзакций
     UpdateTransaction(IndexStore& store);
 
-    //Если Commit() не был вызван, изменения в transaction и transaction_docid
-    //удалятся вместе с этим объектом, а IndexStore разблокируется
+    // Если Commit() не был вызван, изменения в transaction и transaction_docid
+    // удалятся вместе с этим объектом, а IndexStore разблокируется
     ~UpdateTransaction();
 
     // Запрещаем копирование транзакции
-    UpdateTransaction(const UpdateTransaction&) = delete;
-    UpdateTransaction& operator=(const UpdateTransaction&) = delete;
+    UpdateTransaction(const UpdateTransaction&) = delete;            // от UpdateTransaction tx2 = tx1;
+    UpdateTransaction& operator=(const UpdateTransaction&) = delete; // от tx2 = tx1
 
-    //Добавление документа во временную транзакцию.
+    // Добавление документа во временную транзакцию.
     Result<void> AddDocument(size_t doc_id, const std::vector<std::string>& words);
 
-    //Удаление документа из временной транзакции.
+    // Удаление документа из временной транзакции.
     Result<void> RemoveDocument(size_t doc_id);
 
-    //Фиксация изменений Commit.
-    //Переносит данные из transaction и transaction_docid в основной IndexStore.
+    // Фиксация изменений Commit.
+    // Переносит данные из transaction и transaction_docid в основной IndexStore.
     Result<void> Commit();
 
-private:
-    IndexStore& store_ref;          //Ссылка на основной склад IndexStore
+  private:
+    IndexStore& store_ref; // Ссылка на основной склад IndexStore
 
     // Временная копия индекса (её transaction)
     InvertedIndex transaction;
@@ -42,7 +44,7 @@ private:
     // Временный набор ID документов (её transaction_docid)
     std::set<size_t> transaction_docid;
 
-    bool is_committed = false;     // Флаг, был ли вызван Commit
+    bool is_committed = false; // Флаг, был ли вызван Commit
 };
 
 } // namespace lab5::documents
--- src/documents_core/src/UpdateTransaction.cpp
+++ src/documents_core/src/UpdateTransaction.cpp
@@ -2,5 +2,69 @@
 #include <cctype>
 namespace lab5::documents
 {
+UpdateTransaction::UpdateTransaction(IndexStore& store) : store_ref(store)
+{
+    // конструктор сохраняет ссылку на хранилище
+    // transaction и transaction_docid инициализируются по умолчанию (пустые)
+}
+
+UpdateTransaction::~UpdateTransaction()
+{
+    // если коммита не было, разблокируем хранилище
+    if (!is_committed)
+    {
+        store_ref.SetTransactionActive(false);
+    }
+}
+
+Result<void> UpdateTransaction::AddDocument(size_t doc_id, const std::vector<std::string>& words)
+{
+    // проверка, нельзя добавлять документы в уже закоммиченную транзакцию
+    if (is_committed)
+    {
+        return std::unexpected(
+            IndexError{ErrorCode::InternalError, "Cannot add document to already committed transaction"});
+    }
+
+    // добавляем документ во временный индекс и во временный набор ID
+    transaction.AddDocument(doc_id, words);
+    transaction_docid.insert(doc_id);
+
+    return {};
+}
+
+Result<void> UpdateTransaction::RemoveDocument(size_t doc_id)
+{
+    // проверка, нельзя удалять документы из уже закоммиченной транзакции
+    if (is_committed)
+    {
+        return std::unexpected(
+            IndexError{ErrorCode::InternalError, "Cannot remove document from already committed transaction"});
+    }
+
+    // удаляем документ из временного индекса и из временного набора ID
+    transaction.RemoveDocument(doc_id);
+    transaction_docid.erase(doc_id);
+
+    return {};
+}
+
+Result<void> UpdateTransaction::Commit()
+{
+    // роверка, нельзя коммитить дважды
+    if (is_committed)
+    {
+        return std::unexpected(IndexError{ErrorCode::InternalError, "Transaction already committed"});
+    }
+
+    // применяем накопленные изменения к основному хранилищу
+    store_ref.Apply(std::move(transaction), std::move(transaction_docid));
+
+    // разблокируем хранилище и помечаем транзакцию как закоммиченную
+    store_ref.SetTransactionActive(false);
+    is_committed = true;
+
+    return {};
+}
 
 }; // namespace lab5::documents
